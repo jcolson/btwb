8cadbe273bc22319166cc409156930ca04d6da55
jcolson <jay@karma.net>
2015-04-11 17:20:38 +0000

    updated version

--- btwb.php
+++ btwb.php
@@ -35,7 +35,7 @@ function btwb_core_options_page()
 <div style="margin:50px auto; text-align:center;">
 	<h3>Beyond The Whiteboard RSS Fixer</h3>
 	<p>Plugin is loaded - use cron to activate the
-	http://yourwebsite.com/wp-content/plugins/btwb/reparse.php?url=http://beyondthewhiteboard.com/gyms/4860-threefold-crossfit/wods.atom
+	http://yourwebsite.com/wp-content/plugins/btwb/rssparse.php?url=http://beyondthewhiteboard.com/gyms/4860-threefold-crossfit/wods.atom
 	(or something similar) script.  This plugin requires simplepie-core plugin as well.</p>
 </div>
 
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Beyond The Whiteboard RSS Fixer ===
-Contributors: jcolson
+Contributors: jay@Karma.net
 Donate link: http://karma.net
-Tags: rss, atom, feed, feeds, syndication, simplepie, magpie, sidebar, sideblog
+Tags: rss, atom, feed, feeds, syndication, simplepie
 Requires at least: 4.1.1
 Tested up to: 4.1.1
-Stable tag: 1.1.1
+Stable tag: 1.1.2
 
 Combines multiple workouts in a single day to a single event
 
